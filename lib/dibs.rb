e47557cf309a4d944ef9e7db2581fe792966b5f4
Robin Wunderlin <RobWu@users.noreply.github.com>
2016-07-14 12:34:48 +0200

    Issue with DIBS test environment
    Get issues because all transactions will get created as Test-Transactions as long as test is part of the send parameters

--- lib/dibs.rb
+++ lib/dibs.rb
@@ -60,7 +60,11 @@ module Dibs
 
     private
       def do_http_post(opts, endpoint)
-        opts[:test] = 'yes' if opts[:test]
+        if opts[:test]
+          opts[:test] = 'yes'
+        else
+          opts.except!(:test)
+        end
         opts[:textreply] = 'yes'
         opts[:fullreply] = 'yes'
         opts[:postype] = 'ssl'
